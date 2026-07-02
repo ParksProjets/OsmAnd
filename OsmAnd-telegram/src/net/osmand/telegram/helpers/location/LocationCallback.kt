4af3173e43baa5ed3469fab2c213d94e683fe6af
Guillaume Gonnet <ggonnet.linux@gmail.com>
2026-07-02 22:09:33 +0200

    Removed-Folder: OsmAnd-telegram

--- OsmAnd-telegram/src/net/osmand/telegram/helpers/location/LocationCallback.kt
+++
@@ -1,8 +0,0 @@
-package net.osmand.telegram.helpers.location
-
-import net.osmand.Location
-
-abstract class LocationCallback {
-	open fun onLocationResult(locations: List<Location>) { }
-	open fun onLocationAvailability(locationAvailable: Boolean) { }
-}
